c9218bd88f21c71d2d7c1cbbbf5d4fc4a0064b88
Ruben Salazar <rubensalazar@rocketmail.com>
2024-05-16 20:13:50 -0500

    class functions

---
+++ index.js
@@ -0,0 +1,19 @@
+ function receivesAFunction(callback){
+    console.log(callback)
+callback()
+ }
+ 
+ 
+ function returnsANamedFunction(){
+return function name(){
+
+}
+ }
+ 
+
+
+function returnsAnAnonymousFunction () {
+return function (){
+
+}
+}
